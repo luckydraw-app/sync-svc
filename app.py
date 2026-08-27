bf42381461ba6f3be0bb9674349124aad84a750d
Kulbhushan Mayer <kulbhushan.mayer@gmail.com>
2026-08-27 10:29:22 +0530

    updated

--- app.py
+++ app.py
@@ -223,8 +223,10 @@ worker_thread.start()
 
 
 if __name__ == "__main__":
+    import argparse
 
-    app.run(
-        host="0.0.0.0",
-        port=5000
-    )
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--port", type=int, default=5000)
+    args = parser.parse_args()
+
+    app.run(host="0.0.0.0", port=args.port)
